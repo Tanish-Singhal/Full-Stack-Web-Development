92c690d8472525043c2656ee26bfbe5275e8315d
Tanish Singhal <tanishsinghal510@gmail.com>
2023-07-18 01:26:38 +0530

    nothing

---
+++ 05_JavaScript/01_JavaScript/05_truthy_or_flasy.js
@@ -0,0 +1,12 @@
+// In Js, there are two types of values
+// Whether it was true or false
+
+// Falsy values :- null, undefined, "" (empty string), +0, -0, NaN, false
+// Rest all the pther values are true
+
+if ("") {
+    console.log("true");
+}
+else {
+    console.log("false");
+}
