39362d13573aa7277b2698bbdd07b3f29082aa33
Tanish Singhal <tanishsinghal510@gmail.com>
2023-08-04 23:59:29 +0530

    fghfg

--- 05_JavaScript/05_DOM/01_windowObject.js
+++ 05_JavaScript/05_DOM/01_windowObject.js
@@ -16,18 +16,3 @@ let b = 12;
 // if you see in browser "window"
 // then the a = 12 inside the "window" and b = 12 wasn't shown.
 // but it is not good, by this "var" exposses your variable and data to everyone, which is not good. So that's why in let this was not given
-
-
-console.log();
-
-
-// TODO: DOM (Document Object Model)
-// In this all the code of the webiste, whether it was HTML  or CSS code was converted into JS Object
-console.log(document);
-
-
-console.log();
-
-
-// TODO: BOM
-// It allows Js to talk to the browser about the matter or things (like location, history, etc.) other than the content of the page.
--- 05_JavaScript/05_DOM/02_DOM.js
+++ 05_JavaScript/05_DOM/02_DOM.js
@@ -2,3 +2,7 @@
 // In this all the code of the webiste, whether it was HTML  or CSS code was converted into JS Object
 
 console.log(document);
+console.log(document.body);
+
+// How DOM gets created?
+// Character  =>  Tags  =>  Token  =>  Nodes  =>  DOM
