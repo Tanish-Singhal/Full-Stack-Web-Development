b112b0050a052912dfbc7e20f7bd0206384ff055
Tanish Singhal <tanishsinghal510@gmail.com>
2023-08-15 22:34:51 +0530

    Create 08_removingArray.js

---
+++ 05_JavaScript/03_JavaScript.js/08_removingArray.js
@@ -0,0 +1,16 @@
+let arr = [1, 3, 5, 7, 9];
+console.log(arr);
+
+
+// Remove at Beginning of Array
+arr.shift();
+console.log(arr);
+
+// Remove at End of Array 
+arr.pop();
+console.log(arr);
+
+// Remove at Middle 
+arr.splice(1, 2);   // index of the element to be removed, no. of element to be removed.
+console.log(arr);
+console.log();
