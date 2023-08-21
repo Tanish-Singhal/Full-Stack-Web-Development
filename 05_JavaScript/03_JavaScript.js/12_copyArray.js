7da4a13936f71e98a491c439a31a884c0e020a5a
Tanish Singhal <tanishsinghal510@gmail.com>
2023-08-21 19:43:16 +0530

    Create 12_copyArray.js

---
+++ 05_JavaScript/03_JavaScript.js/12_copyArray.js
@@ -0,0 +1,11 @@
+let combined = [1, 2, 3, 4, 5, 6];
+console.log(conbined);
+
+
+// Two methods
+let copyCombined = combined.slice();
+let anotherCombined = [...combined];
+
+console.log(copyCombined);
+console.log(anotherCombined);
+console.log();
